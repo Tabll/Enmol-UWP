4798dc3fdb693a1e8b331cc8c673d198ada7aa0d
Tabll <905153840@qq.com>
2017-11-18 17:58:39 +0800

    增加Dialog接口

---
+++ Enmol/Tools/Dialog.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+
+namespace Enmol.Tools
+{
+    class Dialog : IDialog
+    {
+        public async void ShowSimpleDialog(string title, string content)
+        {
+            ContentDialog myDialog = new ContentDialog()
+            {
+                Title = title,
+                Content = content,
+                PrimaryButtonText = "好的"
+            };
+            ContentDialogResult thisResult = await myDialog.ShowAsync();
+            //throw new NotImplementedException();
+        }
+    }
+}
