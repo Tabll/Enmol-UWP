16b198ffe9fe861df1029a98fb1784e3896fd1c4
Tabll <905153840@qq.com>
2018-01-14 11:43:13 +0800

    实现时间类

--- Enmol/Contents/Calender_Calender.xaml.cs
+++ Enmol/Contents/Calender_Calender.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI;
 using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
@@ -89,6 +90,7 @@ namespace Enmol.Contents
             Grid markerGrid = new Grid
             {
                 Background = todayBrush,
+                Name = "markerGrid"
             };
             weekTitleGrid.Children.Add(markerGrid);
             markerGrid.SetValue(Grid.RowProperty, 3);
@@ -111,5 +113,72 @@ namespace Enmol.Contents
             });
             timer.Start();
         }//时间更新线程
+
+        private void Reflash()
+        {
+            if(weekFromNow == 0)
+            {
+                UpdateDate();
+            }
+            else
+            {
+                Brush brush = new SolidColorBrush(Colors.Black);
+                SUN.Foreground = brush;
+                MON.Foreground = brush;
+                TUE.Foreground = brush;
+                WED.Foreground = brush;
+                THU.Foreground = brush;
+                FRI.Foreground = brush;
+                SAT.Foreground = brush;
+                SUN_DAY.Foreground = brush;
+                MON_DAY.Foreground = brush;
+                TUE_DAY.Foreground = brush;
+                WED_DAY.Foreground = brush;
+                THU_DAY.Foreground = brush;
+                FRI_DAY.Foreground = brush;
+                SAT_DAY.Foreground = brush;
+                SUN_CDAY.Foreground = brush;
+                MON_CDAY.Foreground = brush;
+                TUE_CDAY.Foreground = brush;
+                WED_CDAY.Foreground = brush;
+                THU_CDAY.Foreground = brush;
+                FRI_CDAY.Foreground = brush;
+                SAT_CDAY.Foreground = brush;
+                
+                weekTitleGrid.Children.Remove(FindName("markerGrid") as Grid);
+
+                SUN_DAY.Text = DateTime.Now.AddDays(-((double)DateTime.Now.DayOfWeek) + (weekFromNow * 7)).Day.ToString();
+                MON_DAY.Text = DateTime.Now.AddDays(-((double)DateTime.Now.DayOfWeek) + 1 + (weekFromNow * 7)).Day.ToString();
+                TUE_DAY.Text = DateTime.Now.AddDays(-((double)DateTime.Now.DayOfWeek) + 2 + (weekFromNow * 7)).Day.ToString();
+                WED_DAY.Text = DateTime.Now.AddDays(-((double)DateTime.Now.DayOfWeek) + 3 + (weekFromNow * 7)).Day.ToString();
+                THU_DAY.Text = DateTime.Now.AddDays(-((double)DateTime.Now.DayOfWeek) + 4 + (weekFromNow * 7)).Day.ToString();
+                FRI_DAY.Text = DateTime.Now.AddDays(-((double)DateTime.Now.DayOfWeek) + 5 + (weekFromNow * 7)).Day.ToString();
+                SAT_DAY.Text = DateTime.Now.AddDays(-((double)DateTime.Now.DayOfWeek) + 6 + (weekFromNow * 7)).Day.ToString();
+                SUN_CDAY.Text = BLL.TimeTools.GetChineseDate(DateTime.Now.AddDays(-((double)DateTime.Now.DayOfWeek) + (weekFromNow * 7)));
+                MON_CDAY.Text = BLL.TimeTools.GetChineseDate(DateTime.Now.AddDays(-((double)DateTime.Now.DayOfWeek) + 1 + (weekFromNow * 7)));
+                TUE_CDAY.Text = BLL.TimeTools.GetChineseDate(DateTime.Now.AddDays(-((double)DateTime.Now.DayOfWeek) + 2 + (weekFromNow * 7)));
+                WED_CDAY.Text = BLL.TimeTools.GetChineseDate(DateTime.Now.AddDays(-((double)DateTime.Now.DayOfWeek) + 3 + (weekFromNow * 7)));
+                THU_CDAY.Text = BLL.TimeTools.GetChineseDate(DateTime.Now.AddDays(-((double)DateTime.Now.DayOfWeek) + 4 + (weekFromNow * 7)));
+                FRI_CDAY.Text = BLL.TimeTools.GetChineseDate(DateTime.Now.AddDays(-((double)DateTime.Now.DayOfWeek) + 5 + (weekFromNow * 7)));
+                SAT_CDAY.Text = BLL.TimeTools.GetChineseDate(DateTime.Now.AddDays(-((double)DateTime.Now.DayOfWeek) + 6 + (weekFromNow * 7)));
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = sender as Button;
+            switch (button.Content)
+            {
+                case "<":
+                    weekFromNow -= 1;
+                    break;
+                case ">":
+                    weekFromNow += 1;
+                    break;
+                default:
+                    break;
+            }
+            Reflash();
+        }
     }
 }
