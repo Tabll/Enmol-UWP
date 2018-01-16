b225ef518ed5ee8abe3bde4cb9cca1ec1b729c28
Tabll <905153840@qq.com>
2018-01-16 10:12:22 +0800

    答辩

--- Enmol/Views/EditSchedulePopupWindow.xaml.cs
+++ Enmol/Views/EditSchedulePopupWindow.xaml.cs
@@ -1,27 +1,16 @@
 ﻿using Model;
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Media.Animation;
-using Windows.UI.Xaml.Navigation;
 
 namespace Enmol.Views
 {
     public sealed partial class EditSchedulePopupWindow : UserControl
     {
         private Popup m_Popup;
-        private string m_TextBlockContent;
         private Schedule thisSchedule;
 
         public EditSchedulePopupWindow()
